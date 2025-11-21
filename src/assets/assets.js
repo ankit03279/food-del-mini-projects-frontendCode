e8548a8179690c208c2daea54d3ebabd4042ec2f
Ankit K Gautam <ankitanuj032@gmail.com>
2025-11-21 12:09:35 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,6 @@
 import basket_icon from './basket_icon.png'
 import logo from './logo.png'
+import logo from './logo_5.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
 import menu_1 from './menu_1.png'
@@ -64,6 +65,7 @@ import un_checked from './un_checked.png'
 
 export const assets = {
     logo,
+    logo_5,
     basket_icon,
     header_img,
     search_icon,
